e87c55eac9571f9ba429dd2d0342adef763c460f
darlam-alves <darlamalvessilva@gmail.com>
2024-04-18 14:29:28 -0300

    funcao 5 pontos mais proximos

--- project_ia_tourism.py
+++ project_ia_tourism.py
@@ -1,9 +1,9 @@
 import pandas as pd
-
 tabela = pd.read_csv("pontos_turisticos-rio.csv")
 
 # dicionario com {codigo, nome}
 pontos_turisticos = {}
+
 
 for index, row in tabela.iterrows():
     pontos_turisticos[row["Place ID"]] = row["Name"]
@@ -30,29 +30,53 @@ def criar_grafo(matriz_distancia):
 # Criar o grafo
 grafo = criar_grafo(matriz_distancia)
 
-# Função de busca em profundidade (DFS)
 def dfs(grafo, origem, destino, visitados=None, caminho=None):
     if visitados is None:
         visitados = set()
     if caminho is None:
         caminho = []
+    caminhos_explorados = []  # Adiciona essa linha
     visitados.add(origem)
     caminho.append(origem)
     if origem == destino:
+        caminhos_explorados.append(caminho.copy())  # Adiciona essa linha
         return caminho
     for vizinho in grafo[origem]:
         if vizinho not in visitados:
             novo_caminho = dfs(grafo, vizinho, destino, visitados, caminho)
             if novo_caminho:
+                caminhos_explorados.extend(novo_caminho)  # Adiciona essa linha
                 return novo_caminho
     caminho.pop()
     return None
 
-origem = "Sede da empresa (Ponto inicial)"
-destino = "Templo de Apolo"
-caminho = dfs(grafo, origem, destino)
+# Função para encontrar os 5 pontos mais próximos a partir de cada ponto
+def encontrar_pontos_proximos(df):
+    pontos_proximos = {}
+    for ponto, distancias in df.iterrows():
+        distancias = distancias.str.replace(',', '.').astype(float)  
+        distancias_ordenadas = distancias.sort_values()
+        pontos_mais_proximos = distancias_ordenadas.iloc[1:6]  
+        pontos_proximos[ponto] = pontos_mais_proximos
+    return pontos_proximos
 
-if caminho:
-    print(" -> ".join(caminho))
-else:
-    print("caminho nao encontrado")
+if __name__ == "__main__":
+
+     # Carregar o arquivo CSV
+    df = pd.read_csv('tempo_carro.csv', index_col=0)
+    
+    # Encontrar os 5 pontos mais próximos para cada ponto turístico
+    pontos_proximos = encontrar_pontos_proximos(df)
+
+    origem = "Sede da empresa (Ponto inicial)"
+    destino = "Templo de Apolo"
+    caminho = dfs(grafo, origem, destino)
+
+"""     for ponto, proximos in pontos_proximos.items():
+        print("Os 5 pontos mais próximos a", ponto, "são:")
+        print(proximos) """
+
+    """ if caminho:
+        print(" -> ".join(caminho))
+    else:
+        print("caminho nao encontrado") """
