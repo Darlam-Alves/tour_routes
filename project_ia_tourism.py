4a6086b070fe3fce98c9a00bad61d045cbe676a7
darlam-alves <darlamalvessilva@gmail.com>
2024-04-19 12:19:24 -0300

    ajeitando problema do vizinho ser o destino

--- project_ia_tourism.py
+++ project_ia_tourism.py
@@ -57,13 +57,12 @@ def dfs(grafo, origem, destino, visitados=None, caminho=None):
     caminho.pop()
     return None
 
-# Função para encontrar os 5 pontos mais próximos a partir de cada ponto
 def encontrar_pontos_proximos(df):
     pontos_proximos = {}
     for ponto, distancias in df.iterrows():
         distancias = distancias.str.replace(',', '.').astype(float)  
         distancias_ordenadas = distancias.sort_values()
-        pontos_mais_proximos = distancias_ordenadas.index[1:6]  # Obter apenas os índices (códigos) dos pontos mais próximos
+        pontos_mais_proximos = distancias_ordenadas.index[1:6] 
         pontos_proximos[ponto] = pontos_mais_proximos
     return pontos_proximos
 
@@ -83,31 +82,31 @@ def distancia_real(codigo_origem, codigo_destino):
     matriz_distancias = pd.read_csv("matriz_distancia_carro_km.csv", index_col=0)
     matriz_distancias = matriz_distancias.replace(",", ".", regex=True)  # Substituir vírgulas por pontos
     distancia = pd.to_numeric(matriz_distancias.loc[codigo_origem, codigo_destino], errors='coerce')
-    #print("distancia: ", distancia)
     return distancia
 
 def tempo_real(codigo_origem, codigo_destino):
     matriz_distancias = pd.read_csv("matriz_tempo_carro_horas.csv", index_col=0)
     matriz_distancias = matriz_distancias.replace(",", ".", regex=True)  # Substituir vírgulas por pontos
     tempo = pd.to_numeric(matriz_distancias.loc[codigo_origem, codigo_destino], errors='coerce')
-    #print("tempo: ", tempo)
     return tempo
 
 def distancia_final(codigo_origem, codigo_destino):
     matriz_distancias = pd.read_csv("matriz_distancia_euclidiana.csv", index_col=0)
     matriz_distancias = matriz_distancias.replace(",", ".", regex=True)  # Substituir vírgulas por pontos
     distanciaEuclidiana = pd.to_numeric(matriz_distancias.loc[codigo_origem, codigo_destino], errors='coerce')
-    #print("distanciaEuclidiana: ", distanciaEuclidiana)
     return distanciaEuclidiana
 
 def funcao_de_custo_real(codigo_origem, codigo_destino):
     dReal = distancia_real(codigo_origem, codigo_destino)
+    print("dReal: ", dReal)
     tReal = tempo_real(codigo_origem, codigo_destino)
+    print ("tReal: ", tReal)
     resultado = (dReal*tReal) * (5.93 / 2.215)
     return resultado
 
 def funcao_de_avaliacao(codigo_origem, codigo_destino):  
     dFinal = distancia_final(codigo_origem, codigo_destino)
+    print ("dfinal: ", dFinal)
     resultado = (5.93 / (2.215*90)) * (dFinal * dFinal)
     return resultado
 
@@ -115,36 +114,40 @@ def busca_a_estrela(grafo, nome_origem, nome_destino, pontos_proximos):
     codigo_origem = nome_para_codigo(nome_origem, pontos_turisticos)
     codigo_destino = nome_para_codigo(nome_destino, pontos_turisticos)
 
-
     visitados = set()
     fila_prioridade = PriorityQueue()
     fila_prioridade.put((0, codigo_origem))
 
-    # Dicionário para armazenar o caminho percorrido até cada nó
     caminho = {codigo_origem: None}
 
     while not fila_prioridade.empty():
+        
         custo_total, atual = fila_prioridade.get()
         cod_atual_nome = codigo_para_nome(atual, pontos_turisticos)
 
-        print ("atual: ", cod_atual_nome)
+        #print ("atual: ", cod_atual_nome)
         if atual == codigo_destino:
+            
             print("custo total:", custo_total)
             caminho_percurso = reconstruir_caminho(caminho, codigo_origem, codigo_destino, pontos_turisticos)
             print("Caminho percorrido:", caminho_percurso)
             break
+            
 
         visitados.add(atual)
         vizinhos = pontos_proximos.get(atual, [])
+        print(vizinhos)
+        
         for vizinho in vizinhos:
             if vizinho not in visitados:
+                if vizinho == codigo_destino:
+                    FA = funcao_de_avaliacao(atual, codigo_destino)
+                else:
+                    FA = funcao_de_avaliacao(vizinho, codigo_destino)
                 FC = funcao_de_custo_real(atual, vizinho)
-                FA = funcao_de_avaliacao(vizinho, codigo_destino)
-                custo_total_vizinho = custo_total + FC + FA
-
-                # Atualizar o caminho
+                
+                custo_total_vizinho = FC + FA
                 caminho[vizinho] = atual
-
                 fila_prioridade.put((custo_total_vizinho, vizinho))
 
 def reconstruir_caminho(caminho, origem, destino, pontos_turisticos):
@@ -157,33 +160,16 @@ def reconstruir_caminho(caminho, origem, destino, pontos_turisticos):
     caminho_percurso.append(codigo_para_nome(origem, pontos_turisticos))
     caminho_percurso.reverse()
     return caminho_percurso
-    
-
-
-
-
-
-"""     print("Os 5 pontos mais próximos à origem (", nome_origem, ") são:")
-    for ponto, distancia in pontos_proximos_origem.items():
-        print(ponto, distancia)
-
-    print("\nOs 5 pontos mais próximos ao destino (", nome_destino, ") são:")
-    for ponto, distancia in pontos_proximos_destino.items():
-        print(ponto, distancia) """
-
-   
 
 
 
 if __name__ == "__main__":
-    # Carregar o arquivo CSV
     df = pd.read_csv('matriz_tempo_carro_horas.csv', index_col=0)
     
-    # Encontrar os 5 pontos mais próximos para cada ponto turístico
     pontos_proximos = encontrar_pontos_proximos(df)
 
-    origem = "Sede da empresa (Ponto inicial)"
-    destino = "Cristo Redentor"
+    origem = "Praça da República - Campo de Santana"
+    destino = "Sambódromo Marquês de Sapucaí"
 
     caminho = dfs(grafo, origem, destino)
     busca_a_estrela(grafo, origem, destino, pontos_proximos)
